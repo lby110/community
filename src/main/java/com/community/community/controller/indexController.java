36249b55b06a88e4e51668fd283e20aaf7f27fa7
lby110 <596258311@qq.com>
2020-02-06 15:38:22 +0800

    first commit

---
+++ src/main/java/com/community/community/controller/indexController.java
@@ -0,0 +1,10 @@
+package com.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class indexController {
+    public String index(){
+        return "index";
+    }
+}
